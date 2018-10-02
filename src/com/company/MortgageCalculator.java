48ea53080cac7c370fc02ad78038b98d6ff56e73
cncnewton37@gmail.com <cncnewton37@gmail.com>
2018-10-02 14:54:26 -0400

    Initial commit

---
+++ src/com/company/MortgageCalculator.java
@@ -0,0 +1,44 @@
+package com.company;
+
+	import java.util.Scanner;
+
+        public class MortgageCalculator {
+
+            public static void main(String[] args) {
+
+                Scanner keyboard = new Scanner(System.in);
+                double loan = 0;
+                double interest = 0;
+                double monthlyPayment = 0;
+                double balance = 0;
+                int term;
+
+                System.out.printf("Enter the loan amount: ");
+                loan = keyboard.nextDouble();
+
+                System.out.printf("Enter the interest rate on the loan: ");
+                interest = keyboard.nextDouble();
+
+                System.out.printf("Enter the term (years) for the loan payment: ");
+                term = keyboard.nextInt();
+
+                System.out.printf("\n======================================\n");
+                keyboard.close();
+
+                monthlyPayment = getMonthlyPayment(loan, interest, term);
+                balance = -(monthlyPayment*(term*12));
+                System.out.format("%-30s$%-+10.2f%n", "Balance owed to bank: ", balance);
+                System.out.format("%-30s$%-+10.2f%n", "Minimum monthly payment: ", monthlyPayment);
+            }
+
+
+            public static double getMonthlyPayment(double loan, double interest, int term) {
+                double rate = (interest/ 100) / 12;
+                double base = (loan + rate);
+                double months = term* 12;
+                double result = 0.0;
+                result = ((rate * loan) / (1 - Math.pow(base, -months)));
+
+                return result;
+            }
+        }
